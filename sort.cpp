df40b6c7b4a9877d81a2348801cb8d492a664453
sashafancy <alohalovesweet2014@gmail.com>
2017-03-01 12:44:58 +0800

    correct 3 logic faults along with 1 time improvement

--- sort.cpp
+++ sort.cpp
@@ -40,10 +40,11 @@ void Down(int idx){
 
 int main(){
 	while (~scanf("%d%d", &n,&m)){
+	//while (cin>>n>>m){
 		for (int i = 0; i < m; i++)
 		{
 			scanf("%d", &h[i + 1]);
-			// cin >> h[i + 1];
+			//cin >> h[i + 1];
 		}
 		// Initialization
 		for (int i = 0; i < n / 2; i++)
@@ -51,7 +52,7 @@ int main(){
 
 		for (int i = 0; i < n - m; i++){
 			int temp;
-			// cin >> temp;
+			//cin >> temp;
 			scanf("%d", &temp);
 			if (temp>h[1]){
 				h[1] = temp;
@@ -59,15 +60,17 @@ int main(){
 			}
 		}
 		vector <int>a;
-		for (int i = 1; i <= m; i++){
+		int k = m;
+		for (int i = 1; i <= k; i++){
 			a.push_back(h[1]);
-			h[1] = h[m - i + 1];
-			Down(h[1]);
+			h[1] = h[k - i + 1];
+			Down(1);
+			m--;
 		}
 		int length = a.size();
 		for (int i = 0; i < length - 1; i++){
-			// cout << a[length - i - 1] << " ";
-			printf("%d ", a[length - i + 1]);
+			//cout << a[length - i - 1] << " ";
+			printf("%d ", a[length - i - 1]);
 		}
 		cout << a[0] << endl;
 	}
