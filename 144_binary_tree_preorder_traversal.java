f7f4efaefd90c54d4387ec6f273a986220aa6474
sashafancy <alohalovesweet2014@gmail.com>
2017-09-02 05:53:16 +0800

    leetcode 144 medium tree traversal recursive 21.77%

---
+++ 144_binary_tree_preorder_traversal.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null)
+            return res;
+        res.add(root.val);
+        if(root.left != null)
+            res.addAll(preorderTraversal(root.left));
+        if(root.right != null)
+            res.addAll(preorderTraversal(root.right));
+        return res;
+    }
+}
